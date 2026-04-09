c1bd355a59a05c5978ae0d3ebeb3b5bc79841635
RogerTito455 <crtitofarez@gmail.com>
2026-04-09 21:31:00 +0200

    updated php

--- View/pages/register.php
+++ View/pages/register.php
@@ -40,7 +40,7 @@
       class="register-card"
       role="main"
       aria-labelledby="register-title">
-      <form class="register-form" action="../../Controller/userController->create()">
+      <form action="../../Controller/userController.php" method="POST">
         <div class="form-block">
           <h2 id="register-title">Información personal</h2>
 
