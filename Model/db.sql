ce206ea61f139acc9431d6f1af5e7616f0eb5ee9
israelalmore <israelalmore@gmail.com>
2026-04-16 14:03:27 +0200

    register.php is working and it add's user to db

--- Model/db.sql
+++ Model/db.sql
@@ -12,16 +12,6 @@ INSERT INTO tipos_usuario (nombre) VALUES
 ('Moderador'),
 ('Invitado');
  
-CREATE TABLE idiomas (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50) NOT NULL
-);
-INSERT INTO idiomas (nombre) VALUES
-('Español'),
-('Inglés'),
-('Catalán'),
-('Francés'),
-('Alemán');
  
 CREATE TABLE tipos_documento (
     id INT AUTO_INCREMENT PRIMARY KEY,
@@ -61,7 +51,6 @@ CREATE TABLE usuarios (
     password VARCHAR(255) NOT NULL,
  
     tipo_usuario_id INT NOT NULL,
-    idioma_id INT NOT NULL,
     tipo_documento_id INT NOT NULL,
     direccion_id INT,
  
@@ -69,13 +58,12 @@ CREATE TABLE usuarios (
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
  
     FOREIGN KEY (tipo_usuario_id) REFERENCES tipos_usuario(id),
-    FOREIGN KEY (idioma_id) REFERENCES idiomas(id),
     FOREIGN KEY (tipo_documento_id) REFERENCES tipos_documento(id),
     FOREIGN KEY (direccion_id) REFERENCES direcciones(id)
 );
-INSERT INTO usuarios (nombre, apellido1, apellido2, fecha_nacimiento, email, telefono, documento, password, tipo_usuario_id, idioma_id, tipo_documento_id, direccion_id) VALUES
-('Alejandro', 'García', 'López', '1990-03-15', 'alejandro.garcia@example.com', '612345678', '12345678A', 'password1', 1, 1, 1, 1),
-('Sofía', 'Martínez', 'Pérez', '1985-06-22', 'sofia.martinez@example.com', '622345679', 'X1234567B', 'password2', 2, 2, 2, 2),
-('María', 'Hernández', 'Soler', '1992-11-03', 'maria.hernandez@example.com', '632345680', 'Y2345678C', 'password3', 3, 3, 3, 3),
-('David', 'Ruiz', 'Fernández', '1988-02-10', 'david.ruiz@example.com', '642345681', 'Z3456789D', 'password4', 4, 4, 4, 4),
-('Lucía', 'Santos', 'Gómez', '1995-09-29', 'lucia.santos@example.com', '652345682', 'A4567890E', 'password5', 5, 5, 5, 5);
+INSERT INTO usuarios (nombre, apellido1, apellido2, fecha_nacimiento, email, telefono, documento, password, tipo_usuario_id, tipo_documento_id, direccion_id) VALUES
+('Alejandro', 'García', 'López', '1990-03-15', 'alejandro.garcia@example.com', '612345678', '12345678A', 'password1', 1,  1, 1),
+('Sofía', 'Martínez', 'Pérez', '1985-06-22', 'sofia.martinez@example.com', '622345679', 'X1234567B', 'password2', 2,  2, 2),
+('María', 'Hernández', 'Soler', '1992-11-03', 'maria.hernandez@example.com', '632345680', 'Y2345678C', 'password3', 3, 3, 3),
+('David', 'Ruiz', 'Fernández', '1988-02-10', 'david.ruiz@example.com', '642345681', 'Z3456789D', 'password4', 4,  4, 4),
+('Lucía', 'Santos', 'Gómez', '1995-09-29', 'lucia.santos@example.com', '652345682', 'A4567890E', 'password5', 5, 5, 5);
