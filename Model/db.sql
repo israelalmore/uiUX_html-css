49dc0e412acba002828a3980d93861bb89e9b28b
RogerTito455 <crtitofarez@gmail.com>
2026-04-19 19:43:48 +0200

    Db fixed

--- Model/db.sql
+++ Model/db.sql
@@ -54,5 +54,3 @@ INSERT INTO usuarios (nombre, apellido1, apellido2, fecha_nacimiento, email, tel
 ('David', 'Ruiz', 'Fernández', '1988-02-10', 'david.ruiz@example.com', '642345681', 'Z3456789D', 'password4', 1,  4),
 ('Lucía', 'Santos', 'Gómez', '1995-09-29', 'lucia.santos@example.com', '652345682', 'A4567890E', 'password5', 2, 3);
 
--- AGREGAR COLUMNA DE IMAGEN DE PERFIL
-ALTER TABLE usuarios ADD COLUMN avatar VARCHAR(255) DEFAULT NULL;
