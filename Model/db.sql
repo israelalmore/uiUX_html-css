d1ba234e3f788522f054d7e29eee7fb9798aa269
RogerTito455 <crtitofarez@gmail.com>
2026-04-19 19:33:00 +0200

    fixed pswd

--- Model/db.sql
+++ Model/db.sql
@@ -1,45 +1,48 @@
+DROP DATABASE IF EXISTS forever_events;
 CREATE DATABASE forever_events;
 USE forever_events;
- 
+
 CREATE TABLE tipos_usuario (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(50) NOT NULL
 );
+
 INSERT INTO tipos_usuario (nombre) VALUES
 ('Gestor'),
 ('Usuario');
- 
- 
+
 CREATE TABLE tipos_documento (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(20) NOT NULL
 );
+
 INSERT INTO tipos_documento (nombre) VALUES
 ('DNI'),
 ('NIE'),
 ('Pasaporte'),
 ('Otro');
 
- 
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nombre VARCHAR(100) NOT NULL,
     apellido1 VARCHAR(100) NOT NULL,
     apellido2 VARCHAR(100),
     fecha_nacimiento DATE NOT NULL,
- 
+
     email VARCHAR(150) NOT NULL UNIQUE,
     telefono VARCHAR(20) NOT NULL,
- 
+
     documento VARCHAR(20) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
- 
+
     tipo_usuario_id INT NOT NULL,
     tipo_documento_id INT NOT NULL,
- 
-	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
+    avatar VARCHAR(255) DEFAULT NULL,
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
- 
+
     FOREIGN KEY (tipo_usuario_id) REFERENCES tipos_usuario(id),
     FOREIGN KEY (tipo_documento_id) REFERENCES tipos_documento(id)
 );
